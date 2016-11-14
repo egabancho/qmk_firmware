8ee691ed3cddb43b7d7f06e60e9bc7d0f420d2f2
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2016-11-14 09:09:56 +0100

    Add tap dance (not working)

--- keyboards/ergodox/keymaps/egabancho/keymap.c
+++ keyboards/ergodox/keymaps/egabancho/keymap.c
@@ -1,10 +1,16 @@
 #include "ergodox.h"
 #include "debug.h"
 #include "action_layer.h"
+#include "led.h"
 
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
+
+//Tap Dance Declarations
+enum {
+  TD_SHIFT_CAPS = 0
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -31,24 +37,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,        KC_1,   KC_2,     KC_3,    KC_4,   KC_5,      KC_NO,
-        KC_TAB,        KC_Q,   KC_W,     KC_E,    KC_R,   KC_T,      KC_LBRC,
-        CTL_T(KC_ESC), KC_A,   KC_S,     KC_D,    KC_F,   KC_G,
-        KC_LSFT,       KC_Z,   KC_X,     KC_C,    KC_V,   KC_B,      KC_NO,
-        KC_NO,         KC_NO,  KC_LCTRL, KC_LALT, KC_LGUI,
+        KC_EQL,             KC_1,   KC_2,     KC_3,    KC_4,   KC_5,      KC_NO,
+        KC_TAB,             KC_Q,   KC_W,     KC_E,    KC_R,   KC_T,      KC_LBRC,
+        CTL_T(KC_ESC),      KC_A,   KC_S,     KC_D,    KC_F,   KC_G,
+        TD(TD_SHIFT_CAPS),  KC_Z,   KC_X,     KC_C,    KC_V,   KC_B,      ALL_T(KC_F3),
+        KC_NO,              KC_NO,  KC_LCTRL, KC_LALT, KC_LGUI,
                                                           KC_NO,     KC_NO,
                                                                      KC_HOME,
                                                   KC_SPC, KC_BSPC,   KC_END,
 
         // right hand
-        KC_NUHS,       KC_6,   KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,
-        KC_RBRC,       KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,     KC_BSLS,
-                       KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN,  CTL_T(KC_QUOT),
-        MEH_T(KC_NO),  KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                               KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,
-        KC_NO,         KC_NO,
+        KC_TILD,            KC_6,   KC_7,     KC_8,    KC_9,    KC_0,     KC_MINS,
+        KC_RBRC,            KC_Y,   KC_U,     KC_I,    KC_O,    KC_P,     KC_BSLS,
+                            KC_H,   KC_J,     KC_K,    KC_L,    KC_SCLN,  CTL_T(KC_QUOT),
+        MEH_T(KC_F12),      KC_N,   KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  TD(TD_SHIFT_CAPS),
+                                    KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT, KC_NO,
+        KC_NO,              KC_NO,
         KC_PGUP,
-        KC_PGDN,       KC_TAB, KC_ENT
+        KC_PGDN,            KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -133,6 +139,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_WBAK
 ),
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Shift, twice for Caps Lock
+  [TD_SHIFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS)
 };
 
 const uint16_t PROGMEM fn_actions[] = {
